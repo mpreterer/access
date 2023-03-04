655db99cb089ceafdf6384dc8443ed3e25f45da1
mpreter <dshabelyan@mail.ru>
2023-03-04 18:10:54 +0300

    result

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ class PinCode {
     this.buttons = this.container.querySelectorAll(".js-pin-code-number");
     this.input = this.container.querySelector(".js-pin-code-output");
     this.access = this.container.querySelector(".js-pin-code-access");
+    this.cover = this.container.querySelector(".js-pin-code-cover");
 
     this.initEvents();
   }
@@ -24,9 +25,14 @@ class PinCode {
 
     this.access.addEventListener("click", () => {
       if (this.input.value === "8931") {
-        alert("Ты ж моя умничка!!!");
+        var audio = new Audio();
+        audio.src = "ok.mp3";
+        audio.autoplay = true;
+        this.cover.classList.add("pin-code__cover_visible");
       } else {
-        alert("Попробуй еще");
+        var audio = new Audio();
+        audio.src = "fail.mp3";
+        audio.autoplay = true;
         this.input.value = "";
       }
     });
